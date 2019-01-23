ebb3f1ad48a875494a26e8e57887abb08edb3d60
Dheeraj Bhandaru <bhandaru.dheeraj@gmail.com>
2019-01-23 17:38:06 +0530

    Code file created.

---
+++ GreyAtom/code.py
@@ -0,0 +1,66 @@
+# --------------
+# Code starts here
+class_1 =["Geoffrey Hinton","Andrew Ng","Sebastian Raschka","Yoshua Bengio"]
+class_2 = ["Hilary Mason","Carla Gentry","Corinna Cortes"]
+
+new_class =class_1+class_2
+
+
+new_class.append("Peter Warden")
+
+
+new_class.remove("Carla Gentry")
+print(new_class)
+
+
+# Code ends here
+
+
+# --------------
+# Code starts here
+
+courses = {"Math":65,"English":70,"History":80,"French":70,"Science":60}
+
+
+total = courses["Math"]+courses["English"]+courses["History"]+courses["French"]+courses["Science"]
+
+percentage = (float(total) / 500)*100
+print(percentage)
+
+
+
+# Code ends here
+
+
+# --------------
+# Code starts here
+mathematics ={"Geoffrey Hinton":78,"Andrew Ng":95,"Sebastian Raschka":65,"Yoshua Benjio":50,
+"Hilary Mason":70,"Corinna Cortes":66,"Peter Warden":75}
+import operator
+
+topper = max(mathematics.items(), key=operator.itemgetter(1))[0]
+
+print(topper)
+
+
+
+# Code ends here  
+
+
+# --------------
+# Given string
+topper = 'andrew ng'
+
+
+# Code starts here
+first_name = topper.split(" ")[0]
+last_name = topper.split(" ")[1]
+
+full_name = last_name + " " +first_name
+certificate_name = full_name.upper()
+
+print(certificate_name)
+
+# Code ends here
+
+
